e9faa79b6350c35c1bcc46570a989b01e34dcd7d
Omar <bajunaidomar@gmail.com>
2016-11-22 15:16:26 -0500

    update perform class

--- SymphonyProject1/src/symphony/Perform.java
+++ SymphonyProject1/src/symphony/Perform.java
@@ -1,5 +1,6 @@
 package symphony;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -21,10 +22,10 @@ public class Perform {
 	/**
 	 * overloaded constructor
 	 */
-	public Perform(Soloist soloist,Composition composition, Date datePerformed){
+	public Perform(Soloist soloist,Composition composition){
 		this.soloist = soloist;
 		this.composition = composition;
-		this.datePerformed = datePerformed;
+//		this.datePerformed = datePerformed;
 	}
 	/* ACCESSORS	-----------------------------------------------------*/
 	
@@ -35,7 +36,42 @@ public class Perform {
 	public Soloist getSoloist() {
 		return soloist;
 	}
-
+	
+	/**
+	 * getter for soloist name
+	 * @return the name of the soloist
+	 */
+	public Name getSoilistName(){
+		return soloist.getName();
+	}
+	/**
+	 * getter for the soloist address
+	 * @return the address of the soloist as a string
+	 */
+	public String getSoloistAddress(){
+		return soloist.getAddress().toString();
+	}
+	
+	/**
+	 * getter for the soloist phone number
+	 * @return soloist phone number
+	 */
+	public String getSoloistPhone(){
+		return soloist.getPhoneNumber().getPhoneNum();
+	}
+	
+	/**
+	 * getter for the soloist specialty
+	 * @return soloist specialty
+	 */
+	public String getSoloistSpecialty(){
+		return soloist.getSpecialty();
+	}
+	
+	public int getSoloistid(){
+		return soloist.getId();
+	}
+	
 	/**
 	 * getter for composition
 	 * @return composition
@@ -43,14 +79,30 @@ public class Perform {
 	public Composition getComposition() {
 		return composition;
 	}
-
 	/**
-	 * getter for datePerformed
-	 * @return datePerformed
+	 * getter to get the movements from the composition
+	 * @return movements
 	 */
-	public Date getDatePerformed() {
-		return datePerformed;
+	public ArrayList<Movement> getCompositionMovements(){
+		return composition.getMovements();
 	}
+	/**
+	 * getter to get the composition ID
+	 * @return composition id
+	 */
+	public int getCompositionid(){
+		return composition.getId();
+	}
+	
+	
+
+//	/**
+//	 * getter for datePerformed
+//	 * @return datePerformed
+//	 */
+//	public Date getDatePerformed() {
+//		return datePerformed;
+//	}
 
 	/* MODIFIERS	-----------------------------------------------------*/
 	
@@ -70,13 +122,13 @@ public class Perform {
 		this.soloist = soloist;
 	}
 	
-	/**
-	 * setter for datePerformed
-	 * @param datePerformed
-	 */
-	public void setDatePerformed(Date datePerformed) {
-		this.datePerformed = datePerformed;
-	}
+//	/**
+//	 * setter for datePerformed
+//	 * @param datePerformed
+//	 */
+//	public void setDatePerformed(Date datePerformed) {
+//		this.datePerformed = datePerformed;
+//	}
 	
 	/* NORMAL BEHAVIOR -------------------------------------------------*/
 	
@@ -86,7 +138,7 @@ public class Perform {
 	@Override
 	public String toString() {
 		return "Perform [soloist=" + soloist + ", composition=" + composition
-				+ ", datePerformed=" + datePerformed + "]";
+				+ "]";
 	}
 		
 	/* ATTRIBUTES--------------------------------------------------------*/
@@ -101,10 +153,10 @@ public class Perform {
 	 */
 	private Composition composition;
 	
-	/**
-	 * date that soloist last performed the composition
-	 */
-	private Date datePerformed;
+//	/**
+//	 * date that soloist last performed the composition
+//	 */
+//	private Date datePerformed;
 		
 	/* ENTRY POINT for STAND-ALONE OPERATION ---------------------------*/
 
